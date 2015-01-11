1851381a73ca360b0ab90f3ae72cb726f6808c08
ceberous <cerbus.collin@gmail.com>
2015-01-10 20:02:38 -0500

    Basic Styling and Structure

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :social
+
+  def social
+	  @twitter = "http://twitter.com/ceberous"
+	  @facebook = "http://facebook.com/collin.bourne"
+	  @instagram = "http://instagram.com/ceberous"
+	  @email = "mailto:cerbus.collin@gmail.com"
+  end
+
 end
